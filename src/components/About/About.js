ca122dcdb4f02beff3f5603da95cb1b7991c8bf8
antritue <antritue@gmail.com>
2022-02-06 23:39:48 +0700

    update css

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,39 +18,42 @@ const About = () => {
       <p className="about__desc">{description && description}</p>
 
       <div className="about__contact center">
-        {resume && (
-          <a href={resume} target="_blank">
-            <span type="button" className="btn btn--outline">
-              Resume
-            </span>
-          </a>
-        )}
+        <div className="social">
+          {social && (
+            <>
+              {social.github && (
+                <a
+                  href={social.github}
+                  aria-label="github"
+                  className="link link--icon"
+                  target="_blank"
+                >
+                  <GitHubIcon />
+                </a>
+              )}
 
-        {social && (
-          <>
-            {social.github && (
-              <a
-                href={social.github}
-                aria-label="github"
-                className="link link--icon"
-                target="_blank"
-              >
-                <GitHubIcon />
-              </a>
-            )}
-
-            {social.linkedin && (
-              <a
-                href={social.linkedin}
-                aria-label="linkedin"
-                className="link link--icon"
-                target="_blank"
-              >
-                <LinkedInIcon />
-              </a>
-            )}
-          </>
-        )}
+              {social.linkedin && (
+                <a
+                  href={social.linkedin}
+                  aria-label="linkedin"
+                  className="link link--icon"
+                  target="_blank"
+                >
+                  <LinkedInIcon />
+                </a>
+              )}
+            </>
+          )}
+        </div>
+        <div className="resume">
+          {resume && (
+            <a href={resume} target="_blank">
+              <span type="button" className="btn btn--outline">
+                Resume
+              </span>
+            </a>
+          )}
+        </div>
       </div>
     </div>
   );
